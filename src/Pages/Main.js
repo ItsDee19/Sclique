a2b197c1d26eb1c4e502ec5bc952b7998acd4d43
ItsDee19 <112244900+ItsDee19@users.noreply.github.com>
2023-01-08 06:49:19 +0530

    Update Main.js

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -11,20 +11,6 @@ const Main = () => {
     userName="Vegeks"
     userSecret="1234321"
     />
-    <div className='footer'>
-     <article className="contact__option">
-            <h4>Ask your query</h4>
-            <a href="mailto:dummy@gmail.com" className="btn btn-primary">Send</a>
-           </article>
-           <article className="contact__option">
-            <h4>Share your thoughts</h4>
-            <a href="mailto:dummy@gmail.com" className="btn btn-primary">Send</a>
-           </article>
-           <article className="contact__option">
-            <h4>Post something</h4>
-            <a href="mailto:dummy@gmail.com" className="btn btn-primary">Send</a>
-           </article>
-           </div>
     </div>
   )
 }
